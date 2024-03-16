84f9f912155623eded804366f25b7589757aa5bb
Aaron Blume <aaronmblume@gmail.com>
2024-03-15 22:59:57 -0700

    Update comments in Segformer class

--- src/models/supervised/segformer.py
+++ src/models/supervised/segformer.py
@@ -8,10 +8,10 @@ class Segformer(nn.Module):
     def __init__(self, input_channels, output_channels, scale_factor=50, **kwargs):
         super(Segformer, self).__init__()
         
-        # Create the vision model from config
+        # Create the segformer from pretrained model
         self.model = SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
 
-        # Update the layers in the vision model
+        # Update the layers in the segformer
         self.model.segformer.encoder.patch_embeddings[0].proj = torch.nn.Conv2d(input_channels, 32, kernel_size=(7, 7), stride=(4, 4), padding=(3, 3))
         self.model.decode_head.classifier = torch.nn.Conv2d(256, output_channels, kernel_size=(1, 1), stride=(1, 1))
       
